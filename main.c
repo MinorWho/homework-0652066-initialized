e935f1059080eac57a2e3e0a28f55c28db82a1ce
MinorWho <oormfsfans@gmail.com>
2018-05-18 11:01:19 +0800

    readme

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a = 5;
-	int b = 6;
+	int antGirl = 5;
+	int boyGuys = 6;
 	int s;
-	s = a + b;
-	printf("%d + %d = %d",a,b,s);
+	s = antGirl + boyGuys;
+	printf("%d + %d = %d",antGirl,boyGuys,s);
 	return 0;
 }
