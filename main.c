9db80a8293644230a5d5381ac5b7f0e1bdb7ff7d
MinorWho <oormfsfans@gmail.com>
2018-05-18 10:31:07 +0800

    版本紀錄的說明文字

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 5;
+	int b = 6;
+	int s;
+	s = a + b;
+	printf("%d + %d = %d",a,b,s);
+	return 0;
+}
